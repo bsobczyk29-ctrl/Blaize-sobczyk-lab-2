d4df18cbeea644a9dc87c4e0aaca398b9427348c
Blaize Sobczyk <bsobczyk29@setoncatholic.org>
2026-09-22 18:08:17 +0000

    works the best

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import random
 
 game_number = random.randint(1,10)
-print(game_number)
+while(True):
+    print(game_number)
 
-guess = int(input("enter a number between 1 and 10: "))
+    guess = int(input("enter a number between 1 and 10: "))
 
-if guess > game_number:
-    print("too high")
-elif guess < game_number:
-    print("too low")
-else:
-    print("you win")
+    if guess > game_number:
+        print("too high")
+    elif guess < game_number:
+        print("too low")
+    else:
+        print("you win")
